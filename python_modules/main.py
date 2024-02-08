b2d22494a2f166209c9915f29c6137d5467e8ba3
SirMeows <59692332+SirMeows@users.noreply.github.com>
2024-02-08 12:42:10 +0100

    initial code snippets from class, incomplete

---
+++ python_modules/main.py
@@ -0,0 +1,9 @@
+print("Start of script in main.py file")
+print("imports for this are in the __init__.py file, which runs before any other class")
+print("as a result, we can import the classes on one line")
+print("this in itself is a tiny integration")
+from my_module import ClassA, ClassB
+
+print(file_one)
+
+ClassA()
---
+++ python_modules/my_module/__init__.py
@@ -0,0 +1,3 @@
+print("this file runs before anything else. it contains the imports")
+from my_module.file_one import ClassA
+from my_module.file_two import ClassB
